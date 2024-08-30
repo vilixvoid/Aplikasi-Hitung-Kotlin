43fdc52ae0b0b5c001a08a40afd74bfd5316973d
Mayhesta Gilang Maul <mayhestagilangm@gmail.com>
2024-08-31 01:19:08 +0700

    first commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.myapp
+
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    var angkaBulat: Int = 1
+
+    fun hitung() {
+        angkaBulat++
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val textView: TextView = findViewById(R.id.text_view)
+        val btnHitung: Button = findViewById(R.id.btn_hitung)
+
+        btnHitung.setOnClickListener {
+            hitung()
+            textView.text = angkaBulat.toString()
+        }
+    }
+}
